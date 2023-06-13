a0df3c0b661f288ac9ea74ac0988dbd3525a6ff5
Tizhe82 <tizhepaul@gmail.com>
2023-06-13 02:38:29 -0700

    removed dependencies

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,20 +1,32 @@
 import React, { useContext } from 'react';
 import { ContextCalc } from '../context/ContextCalc';
-import { Textfit } from 'react-textfit';
 
 const Display = () => {
   const { calc } = useContext(ContextCalc);
 
-  const displayContent = calc.num ? calc.num : (calc.res + calc.sign);
+  const displayContent = calc.num ? calc.num : calc.res + calc.sign;
   const displayResult = calc.isEqualClicked ? calc.res : displayContent;
 
   return (
-    <div>
-      <Textfit className='display' max={70} mode='single'>
-        {displayResult}
-      </Textfit>
+    <div className="display" style={styles.display}>
+      {displayResult}
     </div>
   );
+};
+
+const styles = {
+  display: {
+    fontSize: '40px',
+    fontWeight: 'bold',
+    textAlign: 'right',
+    padding: '10px',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+    textOverflow: 'ellipsis',
+    maxWidth: '70px',
+  },
 };
 
 export default Display;
