92fff0bef3f5c77dafae1c55e019d849d78da12c
Tizhe82 <tizhepaul@gmail.com>
2023-06-14 00:36:16 -0700

    updated the display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,29 +4,14 @@ import { ContextCalc } from '../context/ContextCalc';
 const Display = () => {
   const { calc } = useContext(ContextCalc);
 
-  const displayContent = calc.num ? calc.num : calc.res + calc.sign;
+  const displayContent = calc.num ? calc.num : (calc.res + calc.sign);
   const displayResult = calc.isEqualClicked ? calc.res : displayContent;
 
   return (
-    <div className="display" style={styles.display}>
+    <div className="display">
       {displayResult}
     </div>
   );
-};
-
-const styles = {
-  display: {
-    fontSize: '40px',
-    fontWeight: 'bold',
-    textAlign: 'right',
-    padding: '10px',
-    border: '1px solid #ccc',
-    borderRadius: '5px',
-    overflow: 'hidden',
-    whiteSpace: 'nowrap',
-    textOverflow: 'ellipsis',
-    maxWidth: '70px',
-  },
 };
 
 export default Display;
